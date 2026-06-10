cf05d3c8d76551a6cd51787faa8b22e8576416d3
shindevaish <vaishnavi.230805@gmail.com>
2026-06-10 20:26:50 +0530

    create patient

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, Path, HTTPException, Query
-from pydantic import BaseModel, Field
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel, Field, computed_field
 from typing import Annotated, Literal
 import json
 
@@ -14,11 +15,32 @@ class Patient(BaseModel):
     height: Annotated[float, Field(..., gt = 0, description = 'Height of the patient in mtrs')]
     weight: Annotated[float, Field(..., gt = 0, description = 'Weight of the patient in kgs')]
 
+    @computed_field
+    @property
+    def bmi(self) -> float:
+        return round(self.weight / (self.height ** 2), 2)
+
+    @computed_field
+    @property
+    def verdict(self) -> str:
+        if self.bmi < 18.5:
+            return 'Underweight'
+        elif self.bmi < 25:
+            return 'Normal weight'
+        elif self.bmi < 30:
+            return 'Overweight'
+        else:
+            return 'Obese'
+
 app = FastAPI()
 
 def load_data():
     with open('patients.json', 'r') as f:
         return json.load(f)
+
+def save_data(data):
+    with open('patients.json', 'w') as f:
+        json.dump(data, f)
 
 @app.get("/")
 def hello():
@@ -59,3 +81,22 @@ def sort_patients(sort_by: str = Query(..., description = 'Sort on the basis of 
     sorted_data = sorted(data.values(), key = lambda x: x.get(sort_by, 0), reverse = order)
 
     return sorted_data
+
+@app.post('/create')
+def create_patient(patient: Patient):
+    
+    # load existing data
+    data = load_data()
+
+    # check if patient with same id already exists
+    if patient.id in data:
+        raise HTTPException(status_code = 400, detail = f"Patient with ID {patient.id} already exists")
+
+    # add new patient to data
+    data[patient.id] = patient.model_dump(exclude = ['id'])
+
+    # save into data
+    save_data(data)
+
+    # return success message with patient id
+    return JSONResponse(status_code = 201, content = {'message': f"Patient with ID {patient.id} created successfully"})
