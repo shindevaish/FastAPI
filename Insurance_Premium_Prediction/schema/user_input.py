a0f18c248ae4b348b3d3fc76ddec13e95b3de58e
shindevaish <vaishnavi.230805@gmail.com>
2026-06-21 00:21:19 +0530

    remove city tier

--- Insurance_Premium_Prediction/schema/user_input.py
+++ Insurance_Premium_Prediction/schema/user_input.py
@@ -1,15 +1,6 @@
 from pydantic import BaseModel, computed_field, Field, field_validator
 from typing import Literal, Annotated
-
-tier_1_cities = ["Mumbai", "Delhi", "Bangalore", "Chennai", "Kolkata", "Hyderabad", "Pune"]
-tier_2_cities = [
-    "Jaipur", "Chandigarh", "Indore", "Lucknow", "Patna", "Ranchi", "Visakhapatnam", "Coimbatore",
-    "Bhopal", "Nagpur", "Vadodara", "Surat", "Rajkot", "Jodhpur", "Raipur", "Amritsar", "Varanasi",
-    "Agra", "Dehradun", "Mysore", "Jabalpur", "Guwahati", "Thiruvananthapuram", "Ludhiana", "Nashik",
-    "Allahabad", "Udaipur", "Aurangabad", "Hubli", "Belgaum", "Salem", "Vijayawada", "Tiruchirappalli",
-    "Bhavnagar", "Gwalior", "Dhanbad", "Bareilly", "Aligarh", "Gaya", "Kozhikode", "Warangal",
-    "Kolhapur", "Bilaspur", "Jalandhar", "Noida", "Guntur", "Asansol", "Siliguri"
-]
+from Insurance_Premium_Prediction.config.city_tier import tier_1_cities, tier_2_cities
 
 # pydantic model to validate incoming data
 
