449d34ed41170509715f63d0a285240ea77b5b3a
shindevaish <vaishnavi.230805@gmail.com>
2026-06-21 14:30:09 +0530

    remove ml code

--- Insurance_Premium_Prediction/app.py
+++ Insurance_Premium_Prediction/app.py
@@ -1,15 +1,7 @@
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
-import pickle 
-import pandas as pd
 from Insurance_Premium_Prediction.schema.user_input import UserInput
-
-# import the ml model
-with open('Insurance_Premium_Prediction/model.pkl', 'rb') as f:
-    model = pickle.load(f)
-
-# MLFlow
-MODEL_VERSION = '1.0.0'
+from Insurance_Premium_Prediction.model.predict import predict_output, model, MODEL_VERSION
 
 app = FastAPI()
 
@@ -31,15 +23,14 @@ def health_check():
 @app.post('/predict')
 def predict_premium(data: UserInput):
 
-    input_df = pd.DataFrame([{
+    input_df = {
         'bmi' : data.bmi,
         'age_group' : data.age_group,
         'lifestyle_risk' : data.lifestyle_risk,
         'city_tier' : data.city_tier,
         'income_lpa' : data.income_lpa,
         'occupation' : data.occupation
-    }])
+    }
 
-    prediction = model.predict(input_df)[0]
-
+    prediction = predict_output(input_df)
     return JSONResponse(status_code = 200, content = {'prediction_category' : prediction})
