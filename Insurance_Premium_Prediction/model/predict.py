1f70ded05475d7ca7fbbe655cd842457b02a3054
shindevaish <vaishnavi.230805@gmail.com>
2026-06-21 14:29:19 +0530

    added predict file

---
+++ Insurance_Premium_Prediction/model/predict.py
@@ -0,0 +1,17 @@
+import pickle
+import pandas as pd
+
+# import the ml model
+with open('Insurance_Premium_Prediction/model/model.pkl', 'rb') as f:
+    model = pickle.load(f)
+
+# MLFlow
+MODEL_VERSION = '1.0.0'
+
+def predict_output(user_input : dict):
+
+    input_df = pd.DataFrame([user_input])
+
+    output = model.predict(input_df)[0]
+
+    return output
