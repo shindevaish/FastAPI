36e66102e5fb94e058553cfb95806f5c0213f842
shindevaish <vaishnavi.230805@gmail.com>
2026-06-21 15:41:21 +0530

    add confidence score

--- Insurance_Premium_Prediction/model/predict.py
+++ Insurance_Premium_Prediction/model/predict.py
@@ -8,10 +8,25 @@ with open('Insurance_Premium_Prediction/model/model.pkl', 'rb') as f:
 # MLFlow
 MODEL_VERSION = '1.0.0'
 
+# Get class labels from model
+class_labels = model.classes_.tolist()
+
 def predict_output(user_input : dict):
 
     input_df = pd.DataFrame([user_input])
 
-    output = model.predict(input_df)[0]
+    # Predict the class
+    predicted_class = model.predict(input_df)[0]
 
-    return output
+    # Get probabilities for all classes
+    probabilities = model.predict_proba(input_df)[0]
+    confidence = max(probabilities)
+
+    # Create mapping
+    class_proba = dict(zip(class_labels, map(lambda p: round(p, 4), probabilities)))
+
+    return {
+        "predicted category" : predicted_class,
+        "confidence" : round(confidence, 4),
+        "class probabilities" : class_proba
+    }
