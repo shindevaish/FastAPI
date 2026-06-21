ad0003d033b06489192b06b35ad4f65000564739
shindevaish <vaishnavi.230805@gmail.com>
2026-06-21 15:11:35 +0530

    add try-catch

--- Insurance_Premium_Prediction/app.py
+++ Insurance_Premium_Prediction/app.py
@@ -31,6 +31,9 @@ def predict_premium(data: UserInput):
         'income_lpa' : data.income_lpa,
         'occupation' : data.occupation
     }
-
-    prediction = predict_output(input_df)
-    return JSONResponse(status_code = 200, content = {'prediction_category' : prediction})
+    try:
+        prediction = predict_output(input_df)
+        return JSONResponse(status_code = 200, content = {'prediction_category' : prediction})
+    
+    except Exception as e:
+        return JSONResponse(status_code = 500, content = str(e))
